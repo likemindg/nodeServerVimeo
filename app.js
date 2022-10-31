5d78577850d269c551af1aca67dd437971ef21f7
likemindg <likemind@hanmail.net>
2022-10-31 17:09:47 +0900

    passport

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true})) 
 app.set('view engine', 'ejs')
 
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
 app.use(router)
 
 
--- router/join/index.js
+++ router/join/index.js
@@ -4,6 +4,14 @@ var router = express.Router()
 var path = require('path')
 var mysql = require('mysql')
 
+var passport = require('passport')
+//const { doesNotThrow } = require('assert')
+//var LocalStrategy = require('passport-local');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash')
+
+
+
 // DATABASE SETTING
 var connection = mysql.createConnection({
     host: 'likemind.synology.me',
@@ -16,27 +24,77 @@ connection.connect();
 
 // ROUTER
 router.get('/', function(req, res) {
-    console.log('get join url');
+    var msg;
+    var errMsg = req.flash('error')
+
+    console.log('##################');
+    console.log(errMsg);
+    console.log('##################');    
+
+
+
+    if(errMsg) {
+        msg = errMsg;
+        console.log(errMsg);
+    } else {
+        console.log('22222');
+    }
+
+
+    // console.log('get join url');
     // res.sendFile(path.join(__dirname, '../../public/join.html'))
-    res.render('join.ejs')
+
+    res.render('join.ejs', {'message' : msg})
 })
 
-router.post('/', function(req, res){
-    var body = req.body;
-    var email = body.email;
-    var name = body.name;
-    var passwd = body.password;
+passport.use('local-join', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback : true
+    }, function(req, email, password, done) {
 
-    var sql = {email : email, name : name, pw : passwd}
-    var query = connection.query('insert into USER set ?', sql, function(err, rows) {
-        if(err) throw err;
+        
+        var query = connection.query('select * from USER where email=?', [email], function(err, rows){
 
-        console.log("ok db insert");
-        // console.log(rows);
-        res.render('welcome.ejs', {'name' : name})
+            //if(err) throw done(err);
+            if (err) { return done(err); }
 
-    })
-})
+            console.log(rows);
+
+            if(rows.length) {
+                console.log('existed user ======>')
+                return done(null,false,{ message : '가입되지 않았습니다....' })
+            } else {
+                
+            }
+
+            
+        })
+    }
+))
+
+router.post('/', passport.authenticate('local-join', {
+    successRedirect: '/main',
+    failureRedirect: '/join',
+    failureFlash: true })
+)
+
+// router.post('/', function(req, res){
+//     var body = req.body;
+//     var email = body.email;
+//     var name = body.name;
+//     var passwd = body.password;
+
+//     var sql = {email : email, name : name, pw : passwd}
+//     var query = connection.query('insert into USER set ?', sql, function(err, rows) {
+//         if(err) throw err;
+
+//         console.log("ok db insert");
+//         // console.log(rows);
+//         res.render('welcome.ejs', {'name' : name})
+
+//     })
+// })
 
 
 module.exports = router;
